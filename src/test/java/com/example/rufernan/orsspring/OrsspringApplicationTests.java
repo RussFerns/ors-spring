a151e16e9c6d42b8880471d82408467af161ee97
RussFerns <russferns@gmail.com>
2022-08-19 20:48:21 -0400

    original version

---
+++ src/test/java/com/example/rufernan/orsspring/OrsspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rufernan.orsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrsspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
